a5b8148ee4333f9fde338823b7fa09733a6a2320
rahat-495 <md8702687@gmail.com>
2024-10-03 21:20:15 +0600

    search functionality are ok !

---
+++ Controller/searchUser.js
@@ -0,0 +1,18 @@
+
+const UserModel = require("../Models/UserModel");
+
+const searchUser = async (req , res) => {
+    try {
+        
+        const {search} = req.body ;
+        console.log(search)
+        const query = new RegExp(search , "i" , "g") ;
+        const user = await UserModel.find({ "$or" : [ { name : query} , { email : query } ]}) ;
+        return res.send(user)
+        
+    } catch (error) {
+        return res.send({message : error.message || error , error : true}) ;
+    }
+};
+
+module.exports = searchUser ;
--- Routes/index.js
+++ Routes/index.js
@@ -6,6 +6,7 @@ const checkPassword = require("../Controller/checkPassword");
 const userDetails = require("../Controller/userDetails");
 const logOut = require("../Controller/logOut");
 const updateUserDetails = require("../Controller/updateUserDetails");
+const searchUser = require("../Controller/searchUser");
 const router = express.Router() ;
 
 router.post('/register' , registerUser) ; // to register an user ---------
@@ -14,5 +15,6 @@ router.post('/checkPassword' , checkPassword) ; // to check the user pass ------
 router.get('/userDetails' , userDetails) ; // to get user details ---------
 router.get('/logOut' , logOut) ; // to logOut the user ---------
 router.post('/updateUser' , updateUserDetails) ; // to update the user details ---------
+router.post('/searchUser' , searchUser) ; // to get the users ---------
 
 module.exports = router ;
